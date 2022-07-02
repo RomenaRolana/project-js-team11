7c24e073f71a7db20ab2e922cb55a1c6f862639a
Bozhenko Maksym <dvipmakssuper17@gmail.com>
2022-07-02 11:38:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
+import './js/libs';
 import './js/local_storage_operations';
 import './js/modal_in_footer';
-import './js/modal-details';
 import './js/pagination';
 import './js/modal';
 import './js/api_configuration';
